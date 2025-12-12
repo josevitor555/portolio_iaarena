2fcc5e5ef209d041051b8620b3525c9776a419d0
josevitor555 <koservitor123@gmail.com>
2025-12-11 22:10:24 -0300

    Last Update: 11/12/2025

--- iaarena_landinpage/src/components/CTASection.tsx
+++ iaarena_landinpage/src/components/CTASection.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { Mail, Github, Twitter, Linkedin, MessageCircle, FileText } from 'lucide-react';
+import { Mail, FileText } from 'lucide-react';
 
 const CTASection = () => {
     return (
